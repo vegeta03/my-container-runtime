94e6e32e951571c86deec8d2fb995d03bbffc250
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-08-31 11:54:59 +0000

    basic command-line argument parsing

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	msg := "World!!"
-	fmt.Printf("Hello, %[1]s\n", msg)
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <command>")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "run":
+		fmt.Println("Running a container!!!")
+	default:
+		fmt.Println("Unknown command")
+		os.Exit(1)
+	}
 }
