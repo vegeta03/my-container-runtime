3065624e505d6b530662d0d25032c7fcfc1b0cb6
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-09-01 13:12:18 +0000

    restarting

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"os/exec"
 	"syscall"
-
-	"github.com/codeclysm/extract/v4"
 )
 
 func main() {
@@ -32,9 +29,9 @@ func main() {
 func run() {
 	fmt.Printf("Running command: %[1]s\n", os.Args[2:])
 
-	/* 
-	`/proc/self/exe`: This is a special file in Linux systems that represents the currently running executable. 
-	By using this as the command to execute, the program is essentially launching a new instance of itself. 
+	/*
+		`/proc/self/exe`: This is a special file in Linux systems that represents the currently running executable.
+		By using this as the command to execute, the program is essentially launching a new instance of itself.
 	*/
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 
@@ -44,39 +41,17 @@ func run() {
 
 	// Set up the container environment
 	must(syscall.Sethostname([]byte("container")))
-	
-	extractDir := "ubi9.4_rootfs"
-	progress := make(chan float64)
-	go func() {
-	    for p := range progress {
-	        fmt.Printf("Extraction progress: %.2f%%\r", p)
-	    }
-	}()
-	err := extractTar("ubi9.4_rootfs.tar", extractDir)
-	if err != nil {
-	    panic(err)
-	}
 
 	// Use the extracted directory for Chroot
-	must(syscall.Chroot(extractDir))
+	must(syscall.Chroot("/"))
+	must(os.Chdir("/"))
 
-	err = cmd.Run()
+	err := cmd.Run()
 
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-}
-
-func extractTar(src string, dst string) error {
-	file, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	
-	ctx := context.Background()
-	return extract.Tar(ctx, file, dst, nil)
 }
 
 func must(err error) {
